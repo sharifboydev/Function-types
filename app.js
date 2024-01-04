a488700605687e35b76bab76c356e99c18c68997
sharifboy <sharifboy@gmail.com>
2024-01-04 15:14:08 +0500

    added new code

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // Named function
+
+function nameOfFunction(a, b) {
+    return a + b;
+}
+
+console.log(nameOfFunction(10, 20));
+
+
+// Anonymous function
+
+const myFn = function (a, b) {
+    console.log(a, b);
+}
+
+myFn(10, 20)
+
+// Arrow function
+
+const arrFn = (a, b) => {
+    console.log("Arrow function", a, b);
+}
+
+arrFn(50, 60);
+
+
+// Immediately Invoked function expression IIFE
+
+(function() {
+    console.log("IIFE");
+})("asasasas")
+
+
+
+// High order function
+
+
+function randomNumber(min, max) {
+    return Math.round(Math.random() * (max + min) + min)
+}
+
+function highOrderFn(a, b) {
+    console.log(a, b, a + b);
+}
+
+highOrderFn(randomNumber(1, 100), randomNumber(1, 20))
